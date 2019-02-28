3093ad8e89c2de519a917f4594ba7586fc099bad
ashwithaDevireddy <devireddyashwitha@gmail.com>
2019-02-27 22:54:55 -0600

    using methods

---
+++ list_methods.py
@@ -0,0 +1,44 @@
+ dir(list)
+['__add__', '__class__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort'] //displays all methods of the list functions
+
+ address.append("1")
+>>> address
+['flat floor street', 18, 'new york', '1'] //adds the value manually into the list.
+
+
+address.clear()
+>>> address
+[] // clears the list 
+
+
+ address.copy()
+['flat floor street', 18, 'new york'] //copies the list
+
+
+['flat floor street', 18, 'new york', 'new york']
+>>> address.count("new york")
+2  //counts the number of times a value is inserted in the list
+
+
+address.extend("city")
+>>> address
+['flat floor street', 18, 'new york', 'new york', 'c', 'i', 't', 'y']
+// expands the word given manually
+
+
+
+['flat floor street', 18, 'new york', 'new york', 'c', 'i', 't', 'y']
+>>> address.index(18)
+1 // provides the index of the value given manually
+
+
+
+
+
+
+
+
+
+
+
+
