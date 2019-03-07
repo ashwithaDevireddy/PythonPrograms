1af1efd3c6af0ba63b556f267d714fc204d4b4b0
ashwithaDevireddy <devireddyashwitha@gmail.com>
2019-03-06 20:24:26 -0600

    first conditional statement usage

---
+++ conditional.py
@@ -0,0 +1,20 @@
+var=float(input("Enter the number :"))
+
+if var > 50:
+    print("greater")
+elif var == 50:
+    print("equal") // used if multiple conditions are implemented
+else:
+    print("smaller")
+
+
+output:
+Enter the number :2
+smaller
+
+Enter the number :50
+equal
+
+Enter the number :51
+greater
+
