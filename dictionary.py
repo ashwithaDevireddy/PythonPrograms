7ed328aa55ec3d2baf9930645fcaffd45454ae7a
ashwitha97 <devireddyashwitha@gmail.com>
2019-05-19 20:31:37 -0500

    key value pairs

---
+++ dictionary.py
@@ -0,0 +1,2 @@
+y={"fn": "ashwitha", "ln": "Devireddy", "kid": "K00442409", "email": "ashwitha123@gmail.com", "street": "westcorral", "apt": "60", "city": "kingsville", "state": "texas", "zip": "78363", "phone": "361564356", "age": "20", "courses": "databases", "gender" : "female"}
+print(y)
